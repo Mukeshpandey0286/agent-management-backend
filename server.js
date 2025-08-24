ece12c09d762778fe9cca7481fe0d72cc068b2fd
Mukesh <121652157+Mukeshpandey0286@users.noreply.github.com>
2025-08-24 11:31:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,10 +25,7 @@ connectDatabase();
 
 // Middleware
 app.use(
-  cors({
-    origin: "http://localhost:5173", // Vite default port
-    credentials: true,
-  })
+  cors()
 );
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
